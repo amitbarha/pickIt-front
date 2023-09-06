12062228081041b2a6f2c430e8f6b69ee61a8ac3
amitbarha <amitbar131@gmail.com>
2023-09-06 19:15:56 +0300

    add register

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,21 @@ import Splash from "./components/SplashScreen/Splash";
 import RegisterPhone from "./components/Register/RegisterPhone";
 import DeliveryForm from "./components/DeliveryForm/DeliveryForm";
 import Dashboard from "./components/Admin/Dashboard";
+import ConfirmPhone from "./components/Register/ConfirmPhone";
+import RegisterForm from "./components/Register/RegisterForm";
 
 function App() {
   return (
     <div className="app-container">
       <Routes>
-        {/* <Route index element={<Splash />}></Route> */}
+        <Route index element={<Splash />}></Route>
         <Route path="Dashboard" element={<Dashboard />}></Route>
         <Route path="RegisterPhone" element={<RegisterPhone />}></Route>
+        <Route path="ConfirmPhone" element={<ConfirmPhone />}></Route>
+        <Route path="RegisterForm" element={<RegisterForm />}></Route>
         <Route element={<Layout />}>
           <Route path="HomePage" element={<HomePage />}></Route>
-          <Route index element={<DeliveryForm />}></Route>
+          <Route path="FastOrder" element={<DeliveryForm />}></Route>
         </Route>
       </Routes>
     </div>
--- src/components/Register/RegisterPhone.jsx
+++ src/components/Register/RegisterPhone.jsx
@@ -2,35 +2,58 @@ import "./register.css";
 import "react-phone-number-input/style.css";
 import PhoneInput from "react-phone-number-input";
 import { useState } from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import axios from 'axios'
+import { HOST } from "../../utils";
+import Cookies from "js-cookie";
+
 function RegisterPhone() {
   const [phoneNumber, setPhoneNumber] = useState();
+  const navigate = useNavigate()
 
-  const handleSendSMS = () =>{
+  const handleSendSMS = () => {
     console.log(phoneNumber);
-  }
+    axios
+    .post(`${HOST}/users/createUserPhone`, {phoneNumber: phoneNumber})
+    .then(({ data }) => {
+      Cookies.set("phoneNumber", phoneNumber);
+      navigate('/ConfirmPhone')      
+    })
+    .catch((err) => console.log(err.response.data + "basa"));
+  };
 
   return (
-    <div className="register-phone-container">
-      <div className="phone-input-and-text">
-        <h1 className="register-phone-text">
-          Provide your register phone number
-        </h1>
-        <br />
-        <div className="phone-input">
-          {" "}
-          <PhoneInput
-            className="custom-phone-input"
-            placeholder="000 000 0000"
-            defaultCountry="IL"
-            value={phoneNumber}
-            onChange={setPhoneNumber}
-          />
+    <>
+      <Link to={'/'} className="back-arrow">
+        <div className="arrow-img">
+        <img width="30" height="20" src="https://img.icons8.com/ios/50/1A1A1A/forward--v1.png" alt="forward--v1"/>
+        </div>
+        <div className="arrow-text">&nbsp;&nbsp;&nbsp;הרשמה</div>
+      </Link>
+      <div className="register-phone-container">
+        <div className="phone-input-and-text">
+          <h1 className="register-phone-text">הכנס מספר טלפון נייד</h1>
+          <br />
+          <div className="phone-input">
+            {" "}
+            <PhoneInput
+              className="custom-phone-input"
+              placeholder="000 000 0000"
+              defaultCountry="IL"
+              value={phoneNumber}
+              onChange={setPhoneNumber}
+            />
+          </div>
+        </div>
+        <div className="register-phone-button">
+          <button onClick={handleSendSMS} className="blue-button">
+            {" "}
+            הבא
+          </button>
         </div>
       </div>
-      <div className="register-phone-button">
-        <button onClick={handleSendSMS} className="blue-button"> Next</button>
-      </div>
-    </div>
+    </>
   );
 }
 
